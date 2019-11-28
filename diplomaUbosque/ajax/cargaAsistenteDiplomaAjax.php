51c9652027d70c069bd2fd4707ee01a028632ba2
Leonardo <leonardo@UBADMTEC-11.unbosque.edu.co>
2019-11-27 22:12:38 -0500

    Se genera insert funcional a la tabla asistente y asistente diploma

--- diplomaUbosque/ajax/cargaAsistenteDiplomaAjax.php
+++ diplomaUbosque/ajax/cargaAsistenteDiplomaAjax.php
@@ -1,25 +1,36 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 require_once('../../../frames/Excel/reader.php');
+include('../modelo/modeloDiploma.php');
 //require_once(realpath(dirname(__FILE__) .'IOFactory.php'));
 
 if(isset($_POST['diploma']) &&  isset($_FILES)){
-   $idDiploma = $_POST["diploma"];
-    /*Procesamos el file*/
+  $idDiploma = $_POST["diploma"];
+  /*Procesamos el file*/
   $data = new Spreadsheet_Excel_Reader();
   $data->setOutputEncoding('CP1251');
-    /**/
-    $data->read($_FILES['fileAsistente']['tmp_name']);
-    $filas = $data->sheets[0]['numRows'];
+  $data->read($_FILES['fileAsistente']['tmp_name']);
+  $filas = $data->sheets[0]['numRows'];
 for ($z = 2; $z <= $filas; $z++) {
    $numeroDocumento = $fields[$z]['numerodocumento'] = trim($data->sheets[0]['cells'][$z][3]);
    $nombre = $fields[$z]['nombre'] = $data->sheets[0]['cells'][$z][1];
    $apellido =  $fields[$z]['apellidos'] = $data->sheets[0]['cells'][$z][2];
    $tipoDocumento =  $fields[$z]['tipodocumento'] = $data->sheets[0]['cells'][$z][4];
-
-
+   //verifica si el asistente esta registrado o no en el diploma.
+   $cnsRevisaAsistente = diplomaModelo::mdlRevisaAsistenteDiploma($numeroDocumento,$idDiploma);
+//var_dump($cnsRevisaAsistente);
+   if ($tipoDocumento < 10){ $tipoDocumento = "0".$tipoDocumento; }
+   if(count($cnsRevisaAsistente)>0 && $numeroDocumento!=""){//si hay resultados actualiza si no inserta
+        $idAsistente = $cnsRevisaAsistente[0]['idasistente'];
+        diplomaModelo::mdlActualizaAsistente($idAsistente,$nombre,$apellido,$tipoDocumento);
+   }else{
+       $idInsertAsistente = diplomaModelo::mdlInsertaAsistente($nombre,$apellido,$numeroDocumento,$tipoDocumento);//inserta datos en tabla asistente
+       $idAsistente = $idInsertAsistente;
+       $insertaAsistenteDiploma = diplomaModelo::mdlInserAsistenteDiploma($idAsistente,$idDiploma);
+   }
 }
-
-
  /*foreach ($fields as $field)
  {
     echo $field['nombre'];
--- diplomaUbosque/modelo/modeloDiploma.php
+++ diplomaUbosque/modelo/modeloDiploma.php
@@ -8,4 +8,57 @@ class diplomaModelo{
               WHERE nombrediploma LIKE '%".$nombreDiploma."%'";
         return $db->GetAll($sql);
     }
+    public function mdlAsistente($documentoAsistente){
+        $db=Factory::createDbo();
+        $sql="SELECT *
+              FROM asistente 
+              WHERE documentoasistente= '".$documentoAsistente."'";
+        return $db->GetAll($sql);
+    }
+
+    public function mdlRevisaAsistenteDiploma($numeroDocumento,$idDiploma){
+        $db=Factory::createDbo();
+        $sql="SELECT a.idasistente
+              FROM asistente a, asistentediploma ad
+              WHERE   a.idasistente = ad.idasistente AND
+                      a.documentoasistente = '".$numeroDocumento."' AND
+                      ad.iddiploma = '".$idDiploma."' AND
+                      ad.codigoestado=100;";
+        return $db->GetAll($sql);
+    }
+    public function mdlActualizaAsistente($idAsistente,$nombreAsistente,$apellidosAsistente,$tipodocumento){
+        $db=Factory::createDbo();
+        $sql="UPDATE asistente 
+              SET nombreasistente='".utf8_encode($nombreAsistente)."', 
+				  apellidoasistente='".utf8_encode($apellidosAsistente)."',
+				  tipodocumento = '".$tipodocumento."' 
+			  WHERE (idasistente='".$idAsistente."')";
+        return $db->Execute($sql);
+    }
+    public function mdlInsertaAsistente($nombre,$apellidos,$numeroDocumento,$tipoDocumento){
+        $db=Factory::createDbo();
+        $sql="INSERT INTO asistente (nombreasistente, 
+                                     apellidoasistente,
+                                     documentoasistente,
+                                      tipodocumento) 
+                                    VALUES ('".utf8_encode($nombre)."', 
+                                            '".utf8_encode($apellidos)."', 
+                                            '".utf8_encode($numeroDocumento)."', 
+                                            '".utf8_encode($tipoDocumento)."')";
+        $db->Execute($sql);
+        $lastId = $db->insert_Id();
+
+        return $lastId; // retorno el id del insert
+
+    }
+    public function mdlInserAsistenteDiploma($idAsistente,$idDiploma){
+        $db=Factory::createDbo();
+        $sql="INSERT INTO asistentediploma (idasistente, iddiploma, codigoestado)
+              VALUES ('".$idAsistente."', '".$idDiploma."', '100')";
+        return $db->Execute($sql);
+    }
+
+
+
+
 }
